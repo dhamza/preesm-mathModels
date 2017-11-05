0e7ace1c0515dc7e9ed674c5be3b8e5078ed2f6c
HDeroui <HDeroui@insa-rennes.fr>
2017-11-05 12:01:40 +0100

    mapping and scheduling model complete

--- org.ietr.preesm.evaluator.mathModels/src/org/ietr/preesm/evaluator/mathModels/MappingScheduleModel_Gurobi.java
+++ org.ietr.preesm.evaluator.mathModels/src/org/ietr/preesm/evaluator/mathModels/MappingScheduleModel_Gurobi.java
@@ -6,88 +6,168 @@ import gurobi.GRBException;
 import gurobi.GRBLinExpr;
 import gurobi.GRBModel;
 import gurobi.GRBVar;
-import java.util.Hashtable;
-import org.apache.commons.lang3.math.Fraction;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import org.ietr.dftools.algorithm.model.sdf.SDFAbstractVertex;
 import org.ietr.dftools.algorithm.model.sdf.SDFEdge;
 import org.ietr.dftools.algorithm.model.sdf.SDFGraph;
-import org.ietr.dftools.algorithm.model.sdf.SDFInterfaceVertex;
 
 /**
  * @author hderoui
  *
  */
 public class MappingScheduleModel_Gurobi {
-  public Hashtable<String, GRBVar> edgeVar;
+  public HashMap<String, HashMap<Integer, HashMap<Integer, GRBVar>>> varSet_X_a_c_t;
+  public HashMap<String, HashMap<Integer, GRBVar>>                   varSet_S_a_t;
+  public HashMap<String, HashMap<Integer, GRBVar>>                   varSet_R_a_c;
 
   /**
-   * Computes the normalized period K of the SDF graph
+   * compute the optimal solution for the mapping and scheduling problem
    * 
    * @param SDF
    *          graph
-   * 
+   * @param nbCores
+   *          number of resources
+   * @param nbTimeSlot
+   *          time horizon
    */
-  public void computeNormalizedPeriod(SDFGraph SDF) {
+  public void computeNormalizedPeriod(SDFGraph SDF, int nbCores, int nbTimeSlot) {
     // Stopwatch timerPerSche = new Stopwatch();
     // timerPerSche.start();
-
-    edgeVar = new Hashtable<String, GRBVar>(SDF.edgeSet().size());
-    Fraction period = Fraction.getFraction(0.);
+    varSet_X_a_c_t = new HashMap<>(SDF.vertexSet().size());
+    varSet_S_a_t = new HashMap<>(SDF.vertexSet().size());
+    varSet_R_a_c = new HashMap<>(SDF.vertexSet().size());
 
     try {
       // ----- create the Gurobi model ---------------------------------
       GRBEnv env = new GRBEnv();
       GRBModel model = new GRBModel(env);
-      model.set(GRB.StringAttr.ModelName, "Max_Cycle_L/H");
+      model.set(GRB.StringAttr.ModelName, "OptimalMappingScheduling");
 
       // ----- Variables ---------------------------------------------
-      for (SDFEdge e : SDF.edgeSet()) {
-        this.edgeVar.put((String) e.getPropertyBean().getValue("edgeName"),
-            model.addVar(0.0, 1.0, 0.0, GRB.CONTINUOUS, (String) e.getPropertyBean().getValue("edgeName")));
+      // X_a_c_ts variables
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        varSet_X_a_c_t.put(actor.getName(), new HashMap<>());
+        for (int c = 0; c < nbCores; c++) {
+          varSet_X_a_c_t.get(actor.getName()).put(c, new HashMap<>());
+          for (int t = 0; t < nbTimeSlot; t++) {
+            varSet_X_a_c_t.get(actor.getName()).get(c).put(t, model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "X_" + actor.getName() + "_c" + c + "_ts" + t));
+          }
+        }
       }
+
+      // S_a_t variables
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        varSet_S_a_t.put(actor.getName(), new HashMap<>());
+        int Smax = nbTimeSlot - ((int) actor.getPropertyBean().getValue("duration")) + 1;
+        for (int t = 0; t <= Smax; t++) {
+          varSet_S_a_t.get(actor.getName()).put(t, model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "S_" + actor.getName() + "_ts" + t));
+        }
+      }
+
+      // R_a_c varaibles
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        varSet_R_a_c.put(actor.getName(), new HashMap<>());
+        for (int c = 0; c < nbCores; c++) {
+          varSet_R_a_c.get(actor.getName()).put(c, model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "R_" + actor.getName() + "_c" + c));
+        }
+      }
+
       model.update();
 
       // ----- Objectif function -------------------------------------
       GRBLinExpr expr = new GRBLinExpr();
-      for (SDFEdge e : SDF.edgeSet()) {
-        expr.addTerm((Double) e.getSource().getPropertyBean().getValue("duration"), edgeVar.get((String) e.getPropertyBean().getValue("edgeName")));
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        for (Entry<Integer, GRBVar> entry : varSet_S_a_t.get(actor.getName()).entrySet()) {
+          expr.addTerm(entry.getKey(), entry.getValue());
+        }
       }
-      model.setObjective(expr, GRB.MAXIMIZE);
+      model.setObjective(expr, GRB.MINIMIZE);
 
       // ----- Constraints ------------------------------------------
-      // First constraint
-      for (SDFAbstractVertex a : SDF.vertexSet()) {
+      // StartDate constraint: fix(a), var(c,t), Sum(X_a_c_t) >= l.St
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        double dur = (Double) actor.getPropertyBean().getValue("duration");
+        for (Entry<Integer, GRBVar> e : varSet_S_a_t.get(actor.getName()).entrySet()) {
+          expr = new GRBLinExpr();
+          for (int t = e.getKey(); t < e.getKey() + dur; t++) {
+            for (int c = 0; c < nbCores; c++) {
+              expr.addTerm(1.0, varSet_X_a_c_t.get(actor.getName()).get(c).get(t));
+            }
+          }
+          expr.addTerm(-1 * dur, e.getValue());
+          model.addConstr(expr, GRB.GREATER_EQUAL, 0.0, "SC_" + actor.getName() + "_s" + e.getKey());
+        }
+      }
+
+      // Resource constraint: fix(a,c), var(t), Sum(X_a_c_t) <= (=) l.R
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        double dur = (Double) actor.getPropertyBean().getValue("duration");
+        for (int c = 0; c < nbCores; c++) {
+          expr = new GRBLinExpr();
+          for (int t = 0; t < nbTimeSlot; t++) {
+            expr.addTerm(1.0, varSet_X_a_c_t.get(actor.getName()).get(c).get(t));
+          }
+          expr.addTerm(-1 * dur, varSet_R_a_c.get(actor.getName()).get(c));
+          model.addConstr(expr, GRB.LESS_EQUAL, 0.0, "RC_" + actor.getName() + "_c" + c);
+        }
+      }
+
+      // duration constraint: fix(a), var(c,t), Sum(X_a_c_t) = l ===> can be ignored by setting RC = l*R
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        double dur = (Double) actor.getPropertyBean().getValue("duration");
         expr = new GRBLinExpr();
-        // skip the reentrant edges
-        for (SDFInterfaceVertex input : a.getSources()) {
-          SDFEdge e = a.getAssociatedEdge(input);
-          if (e.getSource().getName() != a.getName()) {
-            expr.addTerm(1.0, edgeVar.get((String) e.getPropertyBean().getValue("edgeName")));
+        for (int c = 0; c < nbCores; c++) {
+          for (int t = 0; t < nbTimeSlot; t++) {
+            expr.addTerm(1.0, varSet_X_a_c_t.get(actor.getName()).get(c).get(t));
           }
         }
+        model.addConstr(expr, GRB.EQUAL, dur, "DurC_" + actor.getName());
+      }
 
-        // skip the reentrant edges
-        for (SDFInterfaceVertex output : a.getSinks()) {
-          SDFEdge e = a.getAssociatedEdge(output);
-          if (e.getTarget().getName() != a.getName()) {
-            expr.addTerm(-1.0, edgeVar.get((String) e.getPropertyBean().getValue("edgeName")));
-          }
+      // only one start date need to be activated
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        expr = new GRBLinExpr();
+        for (Entry<Integer, GRBVar> e : varSet_S_a_t.get(actor.getName()).entrySet()) {
+          expr.addTerm(1.0, e.getValue());
         }
-        model.addConstr(expr, GRB.EQUAL, 0.0, a.getName());
+        model.addConstr(expr, GRB.EQUAL, 1.0, "OneExC_" + actor.getName());
       }
 
-      // Second constraint : sum of H.x = 1
-      expr = new GRBLinExpr();
-      for (SDFEdge e : SDF.edgeSet()) {
-        double h = (e.getDelay().intValue() - e.getCons().intValue() + MathFunctionsHelper.gcd(e.getProd().intValue(), e.getCons().intValue()))
-            * (double) e.getPropertyBean().getValue("normalizationFactor");
-        // System.out.println(e.getSource().getName() + " -> " + e.getTarget().getName() + " (" + e.getDelay().intValue() + " - " + e.getCons().intValue() + " +
-        // "
-        // + MathFunctionsHelper.gcd(e.getProd().intValue(), e.getCons().intValue()) + ")" + " * "
-        // + (double) e.getPropertyBean().getValue("normalizationFactor"));
-        expr.addTerm(h, edgeVar.get((String) e.getPropertyBean().getValue("edgeName")));
+      // only one resource need to be used
+      for (SDFAbstractVertex actor : SDF.vertexSet()) {
+        expr = new GRBLinExpr();
+        for (Entry<Integer, GRBVar> e : varSet_R_a_c.get(actor.getName()).entrySet()) {
+          expr.addTerm(1.0, e.getValue());
+        }
+        model.addConstr(expr, GRB.EQUAL, 1.0, "OneResC_" + actor.getName());
       }
-      model.addConstr(expr, GRB.EQUAL, 1.0, "sumHX");
+
+      // each resource execute one actor or less at a time
+      for (int c = 0; c < nbCores; c++) {
+        for (int t = 0; t < nbTimeSlot; t++) {
+          expr = new GRBLinExpr();
+          for (SDFAbstractVertex actor : SDF.vertexSet()) {
+            expr.addTerm(1.0, varSet_X_a_c_t.get(actor.getName()).get(c).get(t));
+          }
+          model.addConstr(expr, GRB.LESS_EQUAL, 1.0, "oneActorPerTSC_" + c + "_t" + t);
+        }
+      }
+
+      // precedence constraints
+      for (SDFEdge edge : SDF.edgeSet()) {
+        expr = new GRBLinExpr();
+        for (Entry<Integer, GRBVar> entry : varSet_S_a_t.get(edge.getTarget().getName()).entrySet()) {
+          expr.addTerm(entry.getKey(), entry.getValue());
+        }
+
+        for (Entry<Integer, GRBVar> entry : varSet_S_a_t.get(edge.getSource().getName()).entrySet()) {
+          expr.addTerm(-1 * entry.getKey(), entry.getValue());
+        }
+
+        model.addConstr(expr, GRB.GREATER_EQUAL, (Double) edge.getSource().getPropertyBean().getValue("duration"),
+            "precC_e_from" + edge.getSource().getName() + "_to_" + edge.getTarget().getName());
+      }
 
       // ----- solve the problem -------------------------------------
       model.optimize();
@@ -95,13 +175,20 @@ public class MappingScheduleModel_Gurobi {
       if (model.get(GRB.IntAttr.Status) == GRB.Status.INFEASIBLE) {
         System.err.println("Model is infeasible or unbounded");
       } else {
-        if (model.get(GRB.DoubleAttr.ObjVal) > 0) {
-          period = Fraction.getFraction(model.get(GRB.DoubleAttr.ObjVal));
+        if (model.get(GRB.DoubleAttr.ObjVal) >= 0) {
+          System.out.println("sum start Date = " + model.get(GRB.DoubleAttr.ObjVal));
         }
+        // print time line for each actor per resource if is activated
+
+        // print the latency
+        // loop the actor get the start date + dur
+        // the max value is the latency
       }
       model.dispose();
       env.dispose();
-    } catch (GRBException e) {
+    } catch (
+
+    GRBException e) {
       e.printStackTrace();
     }
 
